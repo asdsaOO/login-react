e8e246ce412c32976bd96afeb653b074ce1762bd
eco-intern <git-intern@econotec.com.bo>
2024-08-09 16:34:50 -0400

    adsd

--- src/Components/ButtonComp.jsx
+++ src/Components/ButtonComp.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import "../Styles/ButtonComp.css"
+import { useState } from "react";
 
 function ButtonComp({type,text, onclick}){
+  const [loadIndicator,setInd] = useState(false);
+
 
   return (
     <div>
-      <button type={type}>{text}</button>
+      <button type={type} onClick={async()=>{
+        if(onclick){
+          await onclick();
+        }else{
+          console.log("no se asigno un onclick");
+        }
+        setInd(false);
+      }}>
+        <div className="button-content">
+        {loadIndicator?(<img src="./spinner.svg"></img>):text}
+        
+        </div>
+      </button>
     </div>
   )
 }
---
+++ src/Components/SpinnerLoader.jsx
@@ -0,0 +1,11 @@
+import { useEffect,useState } from "react";
+import React from "react";
+function SpinnerLoader(){
+  const [text,setText]= useState('');
+
+  return(
+    <>
+         
+    </>
+  )
+} 
